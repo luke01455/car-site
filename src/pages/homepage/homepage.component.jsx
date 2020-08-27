c85ff282380a7e82464711dd9e548e50f5c4b54f
luke john <luke0145555@hotmail.co.uk>
2020-08-27 20:06:17 +0100

    first commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import './homepage.styles.scss';
+
+function HomePage() {
+  return (
+    <div className="homepage-container">
+    </div>
+  );
+}
+
+export default HomePage;
