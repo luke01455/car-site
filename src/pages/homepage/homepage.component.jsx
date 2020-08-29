5bd1bfe6c97c761b2b1824e633617e825d1bcc55
luke john <luke0145555@hotmail.co.uk>
2020-08-29 21:52:08 +0100

    created background image, added title, positioned them both

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,6 +5,11 @@ import './homepage.styles.scss';
 function HomePage() {
   return (
     <div className="homepage-container">
+      <div className="title-container">
+        <h1>Car Trouble? Find out exactly what's<br/>
+            wrong with your car using Car-Trub.</h1>
+      </div>
+      <div className='contact-page-circle-1'></div>
     </div>
   );
 }
